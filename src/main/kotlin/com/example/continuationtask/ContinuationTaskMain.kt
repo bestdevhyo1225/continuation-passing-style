45444e8533c75d41bfff475bfca5703be80d553a
janghyoseog <bestdevhyo1225@gmail.com>
2022-07-09 20:50:53 +0900

    style: it 대신 continuation 으로 명시

--- src/main/kotlin/com/example/continuationtask/ContinuationTaskMain.kt
+++ src/main/kotlin/com/example/continuationtask/ContinuationTaskMain.kt
@@ -4,22 +4,22 @@ fun main(args: Array<String>) {
     val threads = 10
     val dispatcher = Dispatcher.FixedDispatcher(threads = threads)
     for (i in 0..5) {
-        ContinuationTask(dispatcher = dispatcher, isLazy = false) {
-            when (it.step) {
+        ContinuationTask(dispatcher = dispatcher, isLazy = false) { continuation ->
+            when (continuation.step) {
                 0 -> {
                     println("$i-0 ${Thread.currentThread().id}")
                     Thread.sleep(i * 100L)
-                    it.resume(step = 1)
+                    continuation.resume(step = 1)
                 }
                 1 -> {
                     println("$i-1 ${Thread.currentThread().id}")
                     Thread.sleep(i * 100L)
-                    it.resume(step = 2)
+                    continuation.resume(step = 2)
                 }
                 2 -> {
                     println("$i-2 ${Thread.currentThread().id}")
                     Thread.sleep(i * 100L)
-                    it.complete()
+                    continuation.complete()
                 }
             }
         }
