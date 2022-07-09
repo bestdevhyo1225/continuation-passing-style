3d6466d9ccde491c6fd09cb5e17c79e64617288c
janghyoseog <bestdevhyo1225@gmail.com>
2022-07-09 20:45:31 +0900

    style: Continuation 생성자의 task 인자 명시

--- src/main/kotlin/com/example/continuationtask/Task.kt
+++ src/main/kotlin/com/example/continuationtask/Task.kt
@@ -1,7 +1,7 @@
 package com.example.continuationtask
 
 class Task internal constructor(internal val run: (Continuation) -> Unit){
-    internal val continuation = Continuation(this)
+    internal val continuation = Continuation(task = this)
     internal var isStarted = Status.READY
     internal var isCompleted = Status.READY
     internal var env: MutableMap<String, Any?> = hashMapOf()
